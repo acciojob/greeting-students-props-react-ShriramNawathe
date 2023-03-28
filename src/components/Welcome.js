0dc3aaa5ef875fe52837d557e5cc7bbc2521ae0d
Shriram Rajeshwar Nawathe <108632713+ShriramNawathe@users.noreply.github.com>
2023-03-28 19:40:56 +0530

    sol 1.0

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,15 @@
 // write code for Welcome component here
+import React from "react";
+import "./Welcome.css";
+
+const Welcome = (props) => {
+  const { name } = props;
+  return (
+    <div className="welcome-container">
+      <h1 className="welcome-greeting">Hey {name}!</h1>
+      <h2 className="welcome-subheading">Welcome to school.</h2>
+    </div>
+  );
+};
+
+export default Welcome;
